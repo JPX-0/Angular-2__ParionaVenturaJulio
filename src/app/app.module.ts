38a7eb8d822f855e258f8c2726d53c73fee40029
Julio_Cesar_Pariona_Ventura <Julio_P@hotmail.com>
2022-09-22 21:10:41 -0500

    tarea de la semana 1 del curso de Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { ContainerComponent } from './components/container/container.component';
+import { PaginationComponent } from './components/pagination/pagination.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    ToolbarComponent,
+    ContainerComponent,
+    PaginationComponent
   ],
   imports: [
     BrowserModule
